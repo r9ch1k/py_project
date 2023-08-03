b4990be61f643caba88130734cb07af2fdde1677
vp <mr.r9ch@yandex.ru>
2023-08-03 13:00:27 +0300

    commit №3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from funcs import add_numbers
+from funcs import add_numbers, subtract_numbers
 
 result_sum = add_numbers(5, 3)
-
+result_subtract = subtract_numbers(10, 4)
 
 print("Sum:", result_sum)
-
+print("Subtraction:", result_subtract)
